a840bba5e765b20821d26f31ca6c65d4d93394aa
kulgg <75735874+kulgg@users.noreply.github.com>
2023-06-09 16:32:24 +0200

    Add internationalization

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,13 +2,18 @@ import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
-import "./globals.css";
-import Header from "./Header";
+import "../globals.css";
+import Header from "../Header";
+import { i18n } from "../../i18n-config";
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 const clash = localFont({
-  src: "./ClashDisplay-Semibold.otf",
+  src: "../ClashDisplay-Semibold.otf",
   variable: "--font-clash",
 });
 
@@ -19,11 +24,13 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { lang: string };
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang={params.lang} className="scroll-smooth">
       <body
         className={cn(
           "bg-slate-950 text-white dark font-default",
