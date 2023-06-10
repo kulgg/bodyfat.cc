4dc5bef92470bdfcdb2ebfa23a9beb7dc33353d7
kulgg <75735874+kulgg@users.noreply.github.com>
2023-06-10 17:00:25 +0200

    Fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,27 @@
-export default async function sitemap() {
-  const routes = ["", "/me"].map((route) => ({
-    url: `${process.env.NEXT_PUBLIC_SITE_URL}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
+import { i18n } from "@/i18n-config";
 
-  return [...routes];
+export default async function sitemap() {
+  const paths = ["", "me"];
+  const routes = i18n.locales.flatMap((x) =>
+    paths.map((y) => {
+      if (y === "")
+        return {
+          url: `${process.env.NEXT_PUBLIC_SITE_URL}/${x}`,
+          lastModified: new Date().toISOString().split("T")[0],
+        };
+      else
+        return {
+          url: `${process.env.NEXT_PUBLIC_SITE_URL}/${x}/${y}`,
+          lastModified: new Date().toISOString().split("T")[0],
+        };
+    })
+  );
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+      lastModified: new Date().toISOString().split("T")[0],
+    },
+    ...routes,
+  ];
 }
