c103702b9bbdc229e10d26a8592d622dd65eb243
kulgg <75735874+kulgg@users.noreply.github.com>
2023-06-10 08:38:51 +0200

    Style h1

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import Forms from "./(form)/Forms";
-import History from "./(history)/History";
-import Header from "./Header";
 import { Button } from "@/components/ui/button";
 import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n-config";
+import Balancer from "react-wrap-balancer";
 
 export default async function Home({
   params: { lang },
@@ -16,8 +15,8 @@ export default async function Home({
   return (
     <main className="">
       <div className="px-2 my-8 sm:my-16">
-        <h1 className="text-2xl my-4 tracking-tight text-center">
-          {dictionary.title}
+        <h1 className="text-xl sm:text-xl my-0 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-100 via-green-200 to-rose-500 bg-clip-text text-transparent">
+          <Balancer>{dictionary.title}</Balancer>
         </h1>
         <div className="flex justify-end gap-2">
           <Link href={"/me"}>
